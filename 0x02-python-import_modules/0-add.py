3f2f2f4ca7f604ba5008a91943a5cb162069cfd0
nirmina <nirminahiani12@gmail.com>
2023-11-30 07:35:18 -0800

    task0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add(a, b):
+    res = a + b
+    print(a, "+",b ,"=" ,res)
+    return(res)
+if __name__ == "__main__":
+    add(1, 2)
+else:
+    print("you need to import")
