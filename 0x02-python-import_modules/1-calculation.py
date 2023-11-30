dd583d98de772b28fcc26071bba32f7c319eff0e
nirmina <nirminahiani12@gmail.com>
2023-11-30 08:17:30 -0800

    task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add
     a = 10
     b = 5
-    from calculator_1 import add
     print("{} + {} = {}".format(a, b, add(a, b)))
     from calculator_1 import sub
+    a = 10
+    b = 5
     print("{} - {} = {}".format(a, b, sub(a, b)))
     from calculator_1 import mul
+    a = 10
+    b = 5
     print("{} * {} = {}".format(a, b, mul(a, b)))
     from calculator_1 import div
+    a = 10
+    b = 5
     print("{} / {} = {}".format(a, b, div(a, b)))
